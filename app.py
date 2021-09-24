1ac06a5a9d328ef1858d5368b66fa39198b6fde7
literature <65801567+LiteraturePro@users.noreply.github.com>
2021-09-24 14:18:56 +0800

    1.0

--- app.py
+++ app.py
@@ -143,4 +143,6 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    # app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    # for heroku
+    app.run(debug=True)
